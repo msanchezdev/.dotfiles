429054f1a5ac61a4328c15992ee505f786ba8d45
Manuel Sánchez <manuel.sanchez@stord.com>
2026-03-09 12:37:00 -0400

    Use telescope dropdown

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -27,11 +27,16 @@ return {
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
-      --   { name = "path" }
-      -- }, {
         { name = "cmdline" },
       })
     })
+
+    -- cmp.setup.cmdline({ ':q' }, {
+    --   mapping = cmp.mapping.preset.cmdline({}),
+    --   sources = cmp.config.sources({
+    --     { name = "path" }
+    --   })
+    -- })
 
     cmp.setup.cmdline({ '/', '?' }, {
       mapping = cmp.mapping.preset.cmdline(),
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,20 @@ return {
   config = function()
     local telescope = require('telescope')
     local builtin = require('telescope.builtin')
+    telescope.setup({
+      defaults = {
+        file_ignore_patterns = {
+          'node_modules',
+          '%.git',
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          theme = 'dropdown',
+        }
+      }
+    })
 
     group('Telescope', function(m)
       m.normal('<leader>sh', builtin.help_tags, '[S]earch [H]elp')
@@ -19,17 +33,20 @@ return {
       m.normal('<leader>sr', builtin.resume, '[S]earch [R]esume')
       m.normal('<leader><leader>', builtin.buffers, '[ ] Search Buffers')
 
-      local find_config = function(path)
+      local find_config = function(path, opts)
+        opts = opts or {}
         return function()
-          return builtin.find_files({
+          return builtin.find_files(vim.tbl_deep_extend('force', {
             cwd = '~/.dotfiles/' .. (path or ''),
-            hidden = true,
-            file_ignore_patterns = { '%.git/', '%.lock', '%-lock.json' }
-          })
+          }, opts))
         end
       end
-      m.normal('<leader>scc', find_config(), '[S]earch [C]onfig')
-      m.normal('<leader>scn', find_config('nvim'), '[S]earch [C]onfig / [N]eovim')
+      m.normal('<leader>scc', find_config('', {
+        file_ignore_patterns = { '%.git/', '%-lock.json' }
+      }), '[S]earch [C]onfig')
+      m.normal('<leader>scn', find_config('nvim',{
+        file_ignore_patterns = { '%-lock.json' }
+      }), '[S]earch [C]onfig / [N]eovim')
       m.normal('<leader>sct', find_config('tmux'), '[S]earch [C]onfig / [T]mux')
       m.normal('<leader>scz', find_config('zsh'), '[S]earch [C]onfig / [Z]sh')
     end)
