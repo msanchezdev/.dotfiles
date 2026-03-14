2a3d1ca4364c9f7228cface0ae4d49acb453bc49
Manuel Sánchez <manuel.sanchez@stord.com>
2026-03-13 22:01:46 -0400

    Change background color for active window

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -7,6 +7,7 @@ return {
     config = function()
       -- vim.cmd.colorscheme("gruvbox")
       vim.cmd.colorscheme("monokai-v2")
+      vim.api.nvim_set_hl(0, "Normal", { bg = "#29262a" })
     end
   },
 }
